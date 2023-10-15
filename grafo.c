97b8d4fe9e07ea5e7cc52273571fd44b896ccb54
FernandaRFs <123105473+FernandaRFs@users.noreply.github.com>
2023-10-15 14:36:42 -0300

    Update grafo.c

--- grafo.c
+++ grafo.c
@@ -21,7 +21,6 @@ void criarAresta (grafo *g, int x, int y) {
     if (x > g->vertices || y > g->vertices) {
       g->vertices = tamanho(x,y);
     }
-   
       g->arestas [x][y] = 1;
       g->arestas [y][x] = 1;
 }
@@ -30,12 +29,25 @@ int main () {
   grafo *G = (grafo *)calloc(tam , sizeof (grafo));; 
   FILE *P = fopen("arq.txt", "w");;
      int c, j,i;
-   
-  criarAresta (G, 1, 2);
+   printf(" 0-ma\n 1-pi\n 2-ce\n 3-ba\n 4-pe\n 5-al\n 6-se\n 7-pb\n 8-rn\n");
+criarAresta (G, 0, 1);
+criarAresta (G, 1, 2);
+criarAresta (G, 1, 3);
+criarAresta (G, 1, 4);
+criarAresta (G, 2, 8);
+criarAresta (G, 2, 7);
+criarAresta (G, 2, 4);
+criarAresta (G, 3, 4);
+criarAresta (G, 3, 5);
+criarAresta (G, 3, 6);
+criarAresta (G, 4, 7);
+criarAresta (G, 4, 5);
+criarAresta (G, 5, 6);
+      
 
  
-    for (c=0; c<=2; c++){
-     for(j=0; j<=2;j++)
+    for (c=0; c<=G->vertices; c++){
+     for(j=0; j<=G->vertices;j++)
      printf("%d\n",G->arestas[c][j]);
     }
     
