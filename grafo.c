a583a89ad8dc460e8c83a7b0440cd01e2f2a1307
Armando <113314643+Armandoedu@users.noreply.github.com>
2023-10-16 19:25:17 -0300

    Update grafo.c

--- grafo.c
+++ grafo.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define tam 15 
-
-typedef struct Grafo { 
-    int vertices;
-    int arestas [tam][tam];
-}grafo;
-
-
 int tamanho (int a, int b) {
     if (a > b) { 
       return a;
@@ -44,28 +36,4 @@ void Arquivo(grafo *g){
   }
   
   fclose(arq);
-}
-
-
-
-int main () {
-  grafo *G = (grafo *)calloc(tam , sizeof (grafo));
-
-criarAresta (G, 0, 1);
-criarAresta (G, 1, 2);
-criarAresta (G, 1, 3);
-criarAresta (G, 1, 4);
-criarAresta (G, 2, 8);
-criarAresta (G, 2, 7);
-criarAresta (G, 2, 4);
-criarAresta (G, 3, 4);
-criarAresta (G, 3, 5);
-criarAresta (G, 3, 6);
-criarAresta (G, 4, 7);
-criarAresta (G, 4, 5);
-criarAresta (G, 5, 6);
-criarAresta (G, 7, 8);
-
- Arquivo(G);    
- 
 }
